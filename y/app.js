29419ccfa875636577a647da148a51a375c96a62
Muhammad Ibrahim Alvi <anusnasim911@gmail.com>
2020-07-11 11:05:34 -0700

    stop-watch

---
+++ y/app.js
@@ -0,0 +1,42 @@
+var min = 0;
+var sec = 0;
+var milisec = 0;
+var minheading = document.getElementById("min")
+var secheading = document.getElementById("sec")
+var milisecheading = document.getElementById("milisec")
+var interval;
+function timer(){
+    milisec ++;
+    milisecheading.innerHTML=milisec;
+    if(milisec>=100){
+         sec ++
+         secheading.innerHTML=sec;
+         milisec=0;
+    }
+    else if(sec>=60){
+        min ++;
+        sec=0;
+        minheading.innerHTML=min;
+    }
+}
+
+function start(){
+interval = setInterval(timer,10);
+  document.getElementById("start").disabled=true;
+
+}
+function stop(){
+    clearInterval(interval);
+    document.getElementById("start").disabled=false;
+}
+
+function reset(){
+    min=0;
+    sec=0;
+    milisec=0;
+    minheading.innerHTML=min;
+    secheading.innerHTML=sec;
+    milisecheading.innerHTML=milisec;
+    clearInterval(interval);
+}
+
